d69d2c58c52c27313b83e9570e57a342915f5a60
Indra Wahyu <wicaksana0701@gmail.com>
2023-05-04 11:32:09 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+mkdir ~/.sbash
+cp .bashrc ~/.sbash
+cp .bash_aliases ~/.sbash
+cp emoticon ~/.sbash
+cp basha ~/.sbash
+
+if [ -f ~/.bashrc ]; then
+     rm ~/.bashrc
+     echo "source ~/.sbash/.bashrc" > ~/.bashrc
+fi
+
+touch ~/.sbash/basha/.basha_secret
+
+exec bash -l
